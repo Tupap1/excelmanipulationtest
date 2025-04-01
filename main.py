ee291d3bcb024dd668494779eaab7ab367dacb69
CANTILLO Andres D. TENARIS <ADCANTILLO@tenaris.com>
2025-04-01 13:40:30 -0500

    avance y automatizacion

--- main.py
+++ main.py
@@ -7,18 +7,16 @@ import os
 
 
 
-#informacion = "info2.xlsx"
-template = "template.xlsx"
 logging.basicConfig(filename='registro.log',  
                     level=logging.INFO,    
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
 
+template = "template.xlsx"
+
+
 def parse_data(informacion):
-
-
     arrayinfo = pd.read_excel(informacion, header=3, sheet_name="Resultados", usecols="A,B,C,E,D,J,F,X,Y,Z,AB,AC,AD,AE,AF,AG,AH", engine='openpyxl')
-    
     arrayinfo['Fecha'] = pd.to_datetime(arrayinfo['Fecha'])
 
     columna_clave = None
@@ -30,16 +28,13 @@ def parse_data(informacion):
         columna_clave = 'clave'
 
     arrayordenadoDate = arrayinfo.sort_values(by = ['Fecha', columna_clave], ascending = [False, True])
-
     uniqueMostRecentValues = arrayordenadoDate.groupby(columna_clave).first().reset_index()
-
-    print(uniqueMostRecentValues)
-    print('datos a copiar')
     return uniqueMostRecentValues
 
 
 def convertMmtoIn(x):
     y = x / 25.4
+    y = round(y, 3)
     return y
 
 
@@ -55,61 +50,90 @@ def convTipo(tipo):
     return tipo
 
 
-
 def limiteAbajo( x, tolerancia):
     lim =  x - tolerancia
     return lim
+
 
 def limiteArriba( x,tolerancia):
     lim = x + tolerancia 
     return lim
 
 
-
 def verify_Data(informacion):
-    unmatched_records_in_file = []
     datos = parse_data(informacion)
     datosformateados = datos.to_numpy()
-
+    print(datosformateados)
     
     lookUpValues = pd.read_excel(template, header = 0, engine='openpyxl') 
     
-    #print(lookUpValues)
     try:
         indexArray = 0
         for d in datosformateados:
-                oD =  convertMmtoIn(datosformateados[indexArray,2])
-                wT = convertMmtoInWT(datosformateados[indexArray, 3])
+                oD = convertMmtoIn(datosformateados[indexArray,2])
+                wT = convertMmtoInWT(datosformateados[indexArray,3])
                 tipo = convTipo(datosformateados[indexArray, 1])
                 clave = datosformateados[indexArray,0]
-                grado = datosformateados[indexArray, 4]
+                grado = None
+                if datosformateados[indexArray, 4] == 'N80Q':
+                            grado = 'N80'
+                elif datosformateados[indexArray, 4] == 'P110 ICY' or datosformateados[indexArray, 4] == 'P110 IC' or datosformateados[indexArray, 4] == 'P110 ICCY':
+                            grado = 'P110 ICCY'
+                elif datosformateados[indexArray, 4] == 'L80CR1':
+                            grado = 'L80 CR1'
+                elif datosformateados[indexArray, 4] == 'L80ICY':
+                            grado = 'L80 ICY'
+                elif datosformateados[indexArray, 4] == 'TN110HC':
+                            grado = 'TN110 HC'
+                else: grado = datosformateados[indexArray, 4]
 
-                condicion = (lookUpValues['WT'] >=limiteAbajo(wT, 0.05) )  & (lookUpValues['WT'] <=limiteArriba(wT, 0.05) ) &(lookUpValues['OD'] >=limiteAbajo(oD, 0.5) ) &(lookUpValues['OD'] <=limiteArriba(oD, 0.5) )  & (lookUpValues['TIPO'] == tipo) & (lookUpValues['Clave'] == clave)
-                itemunico = lookUpValues[condicion]
-                print(itemunico)
-                print("obteniendo fila")
-                if itemunico.empty:
-                    unmatched_data = {
+                unmatched_data = {
                         "Clave": clave,
                         "OD": oD,
                         "WT": wT,
                         "Tipo": tipo,
                         "Grado": grado
                     }
+
+
+                condicion = (lookUpValues['WT'] >=limiteAbajo(wT, 0.02) )  & (lookUpValues['WT'] <=limiteArriba(wT, 0.02) ) &(lookUpValues['OD'] >=limiteAbajo(oD, 0.5) ) &(lookUpValues['OD'] <=limiteArriba(oD, 0.5) )  & (lookUpValues['TIPO'] == tipo) & (lookUpValues['Clave'] == clave) &  (lookUpValues['Grado'] == grado) 
+                itemunico = lookUpValues[condicion]
+                print("\n")
+                print("Coincidencias en la template")
+                print("\n")
+                print(itemunico)
+                print("\n")
+                if len(itemunico) > 1:
+                    print(f"Se encontraron muchas coincidencias en la template con estos datos {clave}, {oD}, {wT}, {grado}")
+                    print("deseas omitirlos 1. Si o 2. No")     
+                while True:
+                    opcion = input("Elige una opcion (1 o 2): ")   
+                    if opcion == '1':
+                                print("omitiste este registro")
+                                break  
+                    elif opcion == '2':
+                            print("Saliendo del procesamiento.")
+                            return "por favor edita manualmente el archivo"
+                    else:
+                            print("Opción inválida. Por favor, introduce 1 o 2.")
+                print(d)
+                if itemunico.empty:
+
                     print(d)
-                    print(unmatched_data)
+                    print(unmatched_data, "\n")
                     print("No se encontro una coincidencia de los datos en la template")
                     print("deseas omitirlos 1. Si o 2. No")
                     while True:
                         opcion = input("Elige una opcion (1 o 2): ")
                         if opcion == '1':
-                            print("omitiste este registro")
                             nombre_archivo = os.path.basename(informacion)
+                            rutaArchivo = os.path.dirname(informacion)
                             registro_omitido = f"Clave: {clave}, OD: {oD}, WT: {wT}, Tipo: {tipo}, Grado: {grado}"
                             logging.info(f"Archivo: {nombre_archivo} - Registro omitido (sin coincidencia): {registro_omitido}")
-                            a = open('registrossincoincidencias.txt\n', 'a')
-                            a.write("- " ,registro_omitido, "\n")
+                            a = open('registrossincoincidencias.txt', 'a')
+                            a.write(f"-Values:  {registro_omitido} ---- {rutaArchivo} \n")
                             a.close()
+                            print("omitiste este registro")
                             indexArray = indexArray + 1  
                             break  
                         elif opcion == '2':
@@ -142,20 +166,16 @@ def verify_Data(informacion):
                     logging.info(f"registro agregado:  {asd}" )
                     print("operacion realizada con exito", indicefila)
                     indexArray = indexArray + 1
-                
-                return unmatched_records_in_file
-        
     except Exception as e:
         raise e
     
     
-def procesar_carpeta_excel(ruta_carpeta):
-
+def procesar_carpeta(ruta):
     try:
-        archivos_en_carpeta = os.listdir(ruta_carpeta)
+        archivos_en_carpeta = os.listdir(ruta)
         for nombre_archivo in archivos_en_carpeta:
             if nombre_archivo.endswith((".xlsx", ".xls", ".xlsm")):
-                ruta_completa = os.path.join(ruta_carpeta, nombre_archivo)
+                ruta_completa = os.path.join(ruta, nombre_archivo)
                 print(nombre_archivo)
                 try:
                     verify_Data(ruta_completa)
@@ -168,21 +188,26 @@ def procesar_carpeta_excel(ruta_carpeta):
                         if opcion == '1':
                             print("Omitiendo archivo.")
                             logging.info(f"Archivo: {nombre_archivo} - Omitido debido a error: {e}")
+                            a = open('archivosconerrores.txt', 'a')
+                            a.write( f'- {ruta_completa} \n')
+                            a.close()
                             break  
                         elif opcion == '2':
-                            print("Has seleccionado no omitir. ")
+                            print("Has seleccionado no omitir.")
                             return f"Se encontraron errores. Por favor, revisa el archivo: {nombre_archivo} - Razón: {e}"
                         else:
-                            print("Opción inválida. Por favor, introduce 1 o 2.")
-        print(f"\nProceso completado para todos los archivos Excel en la carpeta: {ruta_carpeta}")
-
+                            print("Opcion inválida")
+        print(f"\nProceso completado para todos los archivos Excel en la carpeta: {ruta}")
+        w = open('carpetasrevisadas.txt','a')
+        w.write(f'- {ruta}\n')
+        w.close()
 
 
     except FileNotFoundError:
-        print(f"Error: No se encontró la carpeta {ruta_carpeta}")
-        return f"Error: No se encontró la carpeta {ruta_carpeta}"
+        print(f"Error: No se encontró la carpeta {ruta}")
+        return f"Error: No se encontró la carpeta {ruta}"
 
     
     
-procesar_carpeta_excel("S:/SEC/TTRTUCA/0- TT04 Master Plan/SPC TT04 2015/SPC TUBING TT04/SPC N80Q Tubing TT04")
-#verify_Data("S:/SEC/TTRTUCA/0- TT04 Master Plan/SPC TT04 2015/SPC CASING TT04/SPC L80 Casing TT04/SPC CL8070362 SMLS  2021 TT04 - Pruebas - .xlsm")
+#procesar_carpeta("S:/SEC/TTRTUCA/0- TT04 Master Plan/SPC TT04 2015/SPC TUBING TT04/SPC J55 Tubing TT04")
+verify_Data('S:/SEC/TTRTUCA/0- TT04 Master Plan/SPC TT04 2015/SPC TUBING TT04/SPC J55 Tubing TT04/SPC TJ3125 2015 TT04.xlsx')
