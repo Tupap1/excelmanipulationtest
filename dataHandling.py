610adf7d0153a3b5c55480649bf0859a1de10e14
CANTILLO Andres D. TENARIS <ADCANTILLO@tenaris.com>
2025-04-04 15:29:58 -0500

    avance nueva funcion FUll PROCESAMIENTO

--- dataHandling.py
+++ dataHandling.py
@@ -20,7 +20,7 @@ def extractData(filename):
 
 
 
-def searchMatches(filename,parameters):
+def searchMatches(filename):
     template = pd.read_excel('template.xlsx', sheet_name ='FULL DATA', header = 2)
     data = extractData(filename)
 
@@ -31,12 +31,38 @@ def searchMatches(filename,parameters):
         grado = item.iloc[3]
         proveedor = item.iloc[5]
         clave =  int(item.iloc[9])
+        item = pd.DataFrame(item)
         
-        
+        ifod = template['OD'] == oD
+        ifwt = template['WT'] == wT
+        ifgrado = template['Grado'] == grado
+        ifclave = template['Clave'] == clave
 
-    
+        ifAll = (ifod & ifwt & ifgrado & ifclave)
+
+        matches = template[ifAll]
+        index = matches.index[0]
+        #matches.loc[index] = item
+        nuevaData = item
+        vCalentamientol1Austenizado = nuevaData.loc['L1 °C']
+        vCalentamientol2Austenizado = nuevaData.loc['L2 °C']
+        vEmpapeL1Austenizado = nuevaData.loc['L1 °C.1']
+        vVelPasaje = nuevaData.loc['m/s']
+        vFlujo = nuevaData.loc['m3/h']
+        vCalentamientol1Revenido = nuevaData.loc['L1 °C.2']
+        vCalentamientol2Revenido = nuevaData.loc['L2 °C.1']
+        vEmpapeL1Revenido = nuevaData.loc['L1 °C.3']
+        vEmpapeL2Revenido = nuevaData.loc['L2 °c']
+        vTCs = nuevaData.loc['seg.1']
+        #print(index)
+
+        template.loc[index, ['Calentamiento L1 austenizado','Calentamiento L2 austenizado','Empape L2 austenizado','Vel. Pasaje','Flujo','Calentamiento L1 revenido','Calentamiento L2 revenido','Empape L1 revenido','Empape L2 revenido','TC [s]']] = [vCalentamientol1Austenizado,vCalentamientol2Austenizado,vEmpapeL1Austenizado,vVelPasaje,vFlujo,vCalentamientol1Revenido,vCalentamientol2Revenido,vEmpapeL1Revenido,vEmpapeL2Revenido,vTCs] 
+        print(item)
+        print('-'*30)
+        print(f'datos copiados con exito en la fila # {index}')
+
+    template.to_excel('templateNew.xlsx', header = True, index = False)
 
 
 
-
-searchMatches('SPC TPC TT04\SPC TPC L80\SPC TPC L80 1\SPC_TPC__93.17_x_12.20_L80_2017_TT04_-_PRUEBAS_(_OD_2_7-8_)_-__-_.xlsm', None)
+searchMatches('SPC TPC TT04\SPC TPC L80\SPC TPC L80 1\SPC_TPC__93.17_x_12.20_L80_2017_TT04_-_PRUEBAS_(_OD_2_7-8_)_-__-_.xlsm')
